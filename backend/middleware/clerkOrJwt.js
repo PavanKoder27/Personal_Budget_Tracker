50cc5123d02d28ddb8c2e0e6c4ab4d57320636e7
Vada Pavan Kalyan <pavankalyan27.career@gmail.com>
2025-09-16 16:42:57 +0530

    Delete backend/middleware/clerkOrJwt.js

--- backend/middleware/clerkOrJwt.js
+++
@@ -1,5 +0,0 @@
-// This file is deprecated and scheduled for removal. Do not import it.
-// If you see this in the repository, remove any references and delete the file.
-module.exports = function () {
-	throw new Error('clerkOrJwt.js is deprecated. Remove its import and use auth.js middleware instead.');
-};
